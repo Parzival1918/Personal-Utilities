cafebe894397fd3ef7b105428654b787de018d4b
Pedro Juan Royo <pedro.juan.royo@gmail.com>
2023-10-03 19:01:55 +0100

    fix: More PermissionError popping up

--- pu_pjr/dir_stats/dir_contents.py
+++ pu_pjr/dir_stats/dir_contents.py
@@ -45,33 +45,38 @@ def find_files_type(dir: pathlib.Path,
         return
 
     for path in paths:
-        if path.is_dir():
-            if search_depth < maximum_depth and not search_full_path and \
-                not (ignore_hidden_dirs and path.name.startswith(".")):
-                find_files_type(path, file_types, search_depth + 1, 
-                                maximum_depth, absolute, search_full_path, 
-                                ignore_hidden_dirs)
-            elif search_full_path \
-                and not (ignore_hidden_dirs and path.name.startswith(".")):
-                find_files_type(path, file_types, search_depth + 1, 
-                                maximum_depth, absolute, search_full_path,
-                                ignore_hidden_dirs)
-        elif path.is_file():
-            if path.suffix in file_types:
-                if absolute:
-                    raw_txt = f"HIT: {path.parent}/{path.name}"
-                else:
-                    # Get file path relative to current working directory
-                    raw_txt = f"HIT: {path.parent.relative_to(pathlib.Path.cwd())}" \
-                    f"/{path.name}"
+        try:
+            if path.is_dir():
+                if search_depth < maximum_depth and not search_full_path and \
+                    not (ignore_hidden_dirs and path.name.startswith(".")):
+                    find_files_type(path, file_types, search_depth + 1, 
+                                    maximum_depth, absolute, search_full_path, 
+                                    ignore_hidden_dirs)
+                elif search_full_path \
+                    and not (ignore_hidden_dirs and path.name.startswith(".")):
+                    find_files_type(path, file_types, search_depth + 1, 
+                                    maximum_depth, absolute, search_full_path,
+                                    ignore_hidden_dirs)
+            elif path.is_file():
+                if path.suffix in file_types:
+                    if absolute:
+                        raw_txt = f"HIT: {path.parent}/{path.name}"
+                    else:
+                        # Get file path relative to current working directory
+                        raw_txt = f"HIT: {path.parent.relative_to(pathlib.Path.cwd())}"\
+                        f"/{path.name}"
 
-                print_txt = Text(raw_txt)
-                # print_txt.highlight_words(file_types, "underline bold")
-                # print_txt.highlight_words(f"{path.parent}/", "cyan")
-                print_txt.stylize("cyan", 4, len(raw_txt) - len(path.name))
-                print_txt.stylize("bold green", len(raw_txt) - len(path.name))
+                    print_txt = Text(raw_txt)
+                    # print_txt.highlight_words(file_types, "underline bold")
+                    # print_txt.highlight_words(f"{path.parent}/", "cyan")
+                    print_txt.stylize("cyan", 4, len(raw_txt) - len(path.name))
+                    print_txt.stylize("bold green", len(raw_txt) - len(path.name))
 
-                print(print_txt)
+                    print(print_txt)
+        except PermissionError as e:
+            text = Text(f"PERMISSION ERROR. {e.filename}", style="red")
+            print(text)
+            continue
                 
 # Find files using a regex expression
 def find_files_expression(dir: pathlib.Path, 
@@ -90,31 +95,37 @@ def find_files_expression(dir: pathlib.Path,
         return
 
     for path in paths:
-        if path.is_dir():
-            if search_depth < maximum_depth and not search_full_path and \
-                not (ignore_hidden_dirs and path.name.startswith(".")):
-                find_files_expression(path, expression, search_depth + 1, 
-                                      maximum_depth, absolute, search_full_path)
-            elif search_full_path \
-                and not (ignore_hidden_dirs and path.name.startswith(".")):
-                find_files_expression(path, expression, search_depth + 1, 
-                                      maximum_depth, absolute, search_full_path)
-        elif path.is_file():
-            #Check that the file name matches the expression
-            # print(path.name, expression)
-            match = re.search(re.escape(expression), path.name)
-            if match:
-                if absolute:
-                    raw_txt = f"HIT: {path.parent}/{path.name}"
-                else:
-                    # Get file path relative to current working directory
-                    raw_txt = f"HIT: {path.parent.relative_to(pathlib.Path.cwd())}" \
-                    f"/{path.name}"
+        try:
+            if path.is_dir():
+                if search_depth < maximum_depth and not search_full_path and \
+                    not (ignore_hidden_dirs and path.name.startswith(".")):
+                    find_files_expression(path, expression, search_depth + 1, 
+                                        maximum_depth, absolute, search_full_path)
+                elif search_full_path \
+                    and not (ignore_hidden_dirs and path.name.startswith(".")):
+                    find_files_expression(path, expression, search_depth + 1, 
+                                        maximum_depth, absolute, search_full_path)
+            elif path.is_file():
+                #Check that the file name matches the expression
+                # print(path.name, expression)
+                match = re.search(re.escape(expression), path.name)
+                if match:
+                    if absolute:
+                        raw_txt = f"HIT: {path.parent}/{path.name}"
+                    else:
+                        # Get file path relative to current working directory
+                        raw_txt = f"HIT: {path.parent.relative_to(pathlib.Path.cwd())}"\
+                        f"/{path.name}"
 
-                print_txt = Text(raw_txt)
-                print_txt.stylize("cyan", 4, len(raw_txt) - len(path.name))
-                print_txt.stylize("green", len(raw_txt) - len(path.name))
-                print_txt.stylize("bold", len(raw_txt) - len(path.name) + match.start(),
-                                  len(raw_txt) - len(path.name) + match.end())
+                    print_txt = Text(raw_txt)
+                    print_txt.stylize("cyan", 4, len(raw_txt) - len(path.name))
+                    print_txt.stylize("green", len(raw_txt) - len(path.name))
+                    print_txt.stylize("bold", len(raw_txt) - len(path.name) + 
+                                      match.start(), 
+                                      len(raw_txt) - len(path.name) + match.end())
 
-                print(print_txt)
+                    print(print_txt)
+        except PermissionError as e:
+            text = Text(f"PERMISSION ERROR. {e.filename}", style="red")
+            print(text)
+            continue
